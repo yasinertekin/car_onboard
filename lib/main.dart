b003faa4af5f60797691e301f4dee58e0d643fd7
Yasin <yasinertekin24@gmail.com>
2024-01-13 18:14:12 +0300

    Remove unused code and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ final class _MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         useMaterial3: true,
       ),
